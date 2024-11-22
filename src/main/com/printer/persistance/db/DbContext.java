27d1bdab0a882e359fb5c0c9025b2cbacdb57341
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-11-22 06:54:21 -0500

    Add db conection

---
+++ src/main/com/printer/persistance/db/DbContext.java
@@ -0,0 +1,20 @@
+package com.printer.persistance.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbContext {
+    //jdbc:mysql://HOST:PORT/DATABASE_NAME
+    private static final String URL="jdbc:mysql://bwxnul4rplitabgcwbv2-mysql.services.clever-cloud.com:3306/bwxnul4rplitabgcwbv2";
+    private static final String USER="u6fgg0zylwlwonvr";
+    private static final String PASSWORD="L9refSY0uOReSlPaCO8w";
+    
+    private DbContext(){}
+    
+    public static Connection  getConeection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    
+    }
+
+}
